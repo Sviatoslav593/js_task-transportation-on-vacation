97118a4a47454b3c6f709bbefd019ea58421d00f
SviatoslavPotapenko <Sviatoslav.Potapenko@lnu.edu.ua>
2025-10-27 13:16:15 +0200

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const sum = days * 40;
+  const MID_TERM = 3;
+  const LONG_TERM = 7;
+  const MID_TERM_DISCOUNT = 20;
+  const LONG_TERM_DISCOUNT = 50;
+
+  if (days >= MID_TERM && days < LONG_TERM) {
+    return sum - MID_TERM_DISCOUNT;
+  }
+
+  if (days >= LONG_TERM) {
+    return sum - LONG_TERM_DISCOUNT;
+  }
+
+  return sum;
 }
 
 module.exports = calculateRentalCost;
